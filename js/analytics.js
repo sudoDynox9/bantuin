38ea715e97f82bc0c5f010d8002ee18f66f2f279
sudoDynox9 <syarifuddinassagaf9@gmail.com>
2026-08-27 16:04:06 +0900

    Create analytics.js

---
+++ js/analytics.js
@@ -0,0 +1,14 @@
+(() => {
+    "use strict";
+
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        window.dataLayer.push(arguments);
+    }
+
+    window.gtag = gtag;
+
+    gtag("js", new Date());
+    gtag("config", "G-8XQLJN1BW");
+})();
